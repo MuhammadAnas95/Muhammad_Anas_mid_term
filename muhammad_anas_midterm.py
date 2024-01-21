ee2df9cb2015366affc7a2839dce09a8cca6bff0
MuhammadAnas95 <134909651+MuhammadAnas95@users.noreply.github.com>
2024-01-21 10:34:02 +0500

    Add files via upload

---
+++ muhammad_anas_midterm.py
@@ -0,0 +1,19 @@
+#Muhammad_Anas
+
+#Q1 Write a Python program to do arithmetical operations addition and division
+
+number1 = 5
+number2 = 8
+print("sum ", number1, " + ",number2)
+
+#Q2 Write a Python program to find the area of a triangle.
+
+b = input("enter the lentgh of the base of the triangle ")
+
+# print ( enter the hieght of triangle lentgh ("enter the number") )
+# input (the sum of("25/26") )
+
+#Q3 Write a Python program to convert Celsius to Fahrenheit.
+
+
+#Q1 Write a Python program that return all datatypes that we discussed in the class.
